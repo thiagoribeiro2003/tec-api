f088837fa77a7deb3d3d667ef7c707b93c1f61dd
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-09-06 09:39:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { ler, inserir, lerUm, atualizar, excluir } from "./src/aluno.js";
 
 const app = express();
 const porta = process.env.PORT || 3000;
+
+// Configurando requisições de diferentes origens
+app.use(cors())
 
 // Configurando suporte ao formato JSON
 app.use(express.json());
